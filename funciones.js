a9a1a421eda211a1ee17de465def3cb3d233ee1a
Lautii0x0109 <youthanasiax86@gmail.com>
2026-08-21 03:20:06 -0300

    Add files via upload

--- funciones.js
+++ funciones.js
@@ -1,11 +1,20 @@
 "use strict";
 const oscuroclaro = document.getElementById("modoco");
-oscuroclaro?.addEventListener("click", () => {
-    document.body.classList.toggle("modo-claro");
-    if (document.body.classList.contains("modo-claro")) {
-        oscuroclaro.textContent = "Modo Oscuro";
-    }
-    else {
-        oscuroclaro.textContent = "Modo Claro";
-    }
-});
+if (oscuroclaro != null) {
+    oscuroclaro.addEventListener("click", () => {
+        document.body.classList.toggle("modo-claro");
+        if (document.body.classList.contains("modo-claro")) {
+            oscuroclaro.textContent = "Modo Oscuro";
+        }
+        else {
+            oscuroclaro.textContent = "Modo Claro";
+        }
+    });
+}
+const intro = document.getElementById("intro");
+const entranding = document.getElementById("entranding");
+if (entranding != null && intro != null) {
+    entranding.addEventListener("click", () => {
+        intro.classList.add("salir");
+    });
+}
--- funciones.ts
+++ funciones.ts
@@ -1,16 +1,33 @@
 const oscuroclaro = document.getElementById("modoco");
 
-oscuroclaro?.addEventListener("click",()=>{
+if (oscuroclaro != null)
+{
+    oscuroclaro.addEventListener("click", () => {
 
-    document.body.classList.toggle("modo-claro");
+        document.body.classList.toggle("modo-claro");
 
-    if(document.body.classList.contains("modo-claro"))
-    {
-        oscuroclaro.textContent = "Modo Oscuro";
-    }
-    else
-    {
-        oscuroclaro.textContent = "Modo Claro";
-    }
+        if (document.body.classList.contains("modo-claro"))
+        {
+            oscuroclaro.textContent = "Modo Oscuro";
+        }
+        else
+        {
+            oscuroclaro.textContent = "Modo Claro";
+        }
 
-});
+    });
+}
+
+const intro = document.getElementById("intro");
+const entranding = document.getElementById("entranding");
+
+
+if(entranding != null && intro != null)
+{
+    entranding.addEventListener("click",() => {
+        
+        intro.classList.add("salir"); 
+    
+    });
+
+}
